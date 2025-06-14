b025e4554ab5c2e7d8890c6cc6b73d7535d1a33c
Ares Stavropoulos <56235072+Ares1605@users.noreply.github.com>
2025-06-13 21:22:14 -0400

    add common zsh commads

---
+++ common.zsh
@@ -0,0 +1,15 @@
+alias n="nvim"
+alias gs="git status"
+alias gl="git pull"
+alias gp="git push"
+alias gc="git commit"
+alias venv="source .venv/bin/activate"
+alias szc="source $HOME/.zshrc"
+
+export nc="$HOME/.config/nvim"
+export kc="$HOME/.config/kitty"
+export zc="$HOME/.zshrc"
+export dc="$HOME/.dotfiles/"
+
+# start github ssh agent
+#eval "$(ssh-agent -s)"
