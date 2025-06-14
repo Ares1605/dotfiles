415a45beb373751e067ce7becee049287e8c27a8
Ares Stavropoulos <aresstav04@gmail.com>
2025-06-14 10:58:30 -0500

    add github agent exec

--- common.zsh
+++ common.zsh
@@ -1,15 +1,18 @@
 alias n="nvim"
+alias v="vim"
 alias gs="git status"
 alias gl="git pull"
 alias gp="git push"
 alias gc="git commit"
 alias venv="source .venv/bin/activate"
-alias szc="source $HOME/.zshrc"
+alias szc="source $zc"
 
+export c="$HOME/.config"
 export nc="$HOME/.config/nvim"
 export kc="$HOME/.config/kitty"
 export zc="$HOME/.zshrc"
 export dc="$HOME/.dotfiles/"
 
-# start github ssh agent
-#eval "$(ssh-agent -s)"
+# add github agent
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/github_key
