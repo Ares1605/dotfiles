7e256e31488a4823c2a380de1c1559cc4de1b0c0
Ares Stavropoulos <aresstav04@gmail.com>
2025-06-16 13:25:22 -0500

    fix breaking swaync client

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -12,7 +12,7 @@ main() {
     selected_wallpaper=$(echo "$choice" | sed 's/^img://')
     swww img "$selected_wallpaper" --transition-type any --transition-fps 60 --transition-duration .5
     wal -i "$selected_wallpaper" -n
-    swaync-client --reload-css
+    # swaync-client --reload-css
     cat ~/.cache/wal/colors-kitty.conf > ~/.config/kitty/current-theme.conf
     pywalfox update
     # cava_config="$HOME/.config/cava/config"
