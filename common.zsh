628530d3540bda317fc7b4906cfb1f1f485a46f3
Ares Stavropoulos <aresstav04@gmail.com>
2025-09-06 16:31:53 -0500

    laptop config

--- common.zsh
+++ common.zsh
@@ -1,4 +1,5 @@
-PS1='%n@%m %~$ '
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
 
 alias n="nvim"
 alias v="vim"
@@ -6,8 +7,19 @@ alias gs="git status"
 alias gl="git pull"
 alias gp="git push"
 alias gc="git commit"
+alias gb="git branch"
+alias gch="git checkout"
 alias venv="source .venv/bin/activate"
 alias szc="source $HOME/.zshrc"
+alias sv="source .venv/bin/activate"
+alias nf="neofetch"
+alias hr="hyprctl reload"
+alias h="Hyprland"
+
+alias i="sudo pacman -S"
+alias is="pacman -Ss"
+
+alias c="wl-copy"
 
 export c="$HOME/.config"
 export nc="$HOME/.config/nvim"
@@ -18,3 +30,5 @@ export dc="$HOME/.dotfiles/"
 # add github agent
 eval "$(ssh-agent -s)" > /dev/null
 ssh-add ~/.ssh/github_key 2>/dev/null
+
+eval "$(starship init zsh)"
