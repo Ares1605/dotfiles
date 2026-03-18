3d0ae37dbf315f18b0e97d8ea573bd7141bb6365
Ares Stavropoulos <aresstav04@gmail.com>
2026-03-18 13:21:49 -0500

    remove FF transition duration

--- firefox-user.js
+++ firefox-user.js
@@ -9,6 +9,8 @@ user_pref("browser.compactmode.show", true);
 user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.contentblocking.category", "standard");
 user_pref("browser.fullscreen.autohide", false);
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
 user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
 user_pref("browser.tabs.allow_transparent_browser", true);
 user_pref("browser.toolbarbuttons.introduced.sidebar-button", true);
