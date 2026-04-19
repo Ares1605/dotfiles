c5ebe6ece939fe1d444524e96dae68268cde4c98
Ares Stavropoulos <aresstav04@gmail.com>
2026-04-19 16:25:59 -0500

    Complete snacks file explorer implementation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,21 +2,77 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 vim.opt.clipboard="unnamedplus"
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
 vim.opt.number = true
 vim.opt.shiftwidth = 4
 vim.opt.wrap = false
 
+-- Every wrapped line will continue visually indented (same amount of
+-- space as the beginning of that line), thus preserving horizontal
+-- blocks of text.
+vim.opt.breakindent = true
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.g.have_nerd_font = true
+vim.o.mouse = 'a' -- Enable mouse support for all modes
+vim.o.showmode = false
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- IDK What sign column is..
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 8
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-guide-options`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Enable undo/redo changes even after closing and reopening a file
+vim.o.undofile = true
+
 vim.keymap.set("n", "<C-S-j>", function()
-    vim.cmd('normal! j')
-    vim.cmd('normal! zz')
+  vim.cmd('normal! j')
+  vim.cmd('normal! zz')
 end)
 vim.keymap.set("n", "<C-S-k>", function()
-    vim.cmd('normal! k')
-    vim.cmd('normal! zz')
+  vim.cmd('normal! k')
+  vim.cmd('normal! zz')
 end)
 
 vim.keymap.set("n", "<C-d>", "15j", { desc = "Page down" })
@@ -33,27 +89,48 @@ vim.keymap.set("n", "<leader>c", "<cmd>bdelete<cr>", { desc = "Close buffer" })
 vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Go to previous buffer" })
 
 vim.keymap.set("n", "]e", function()
-    vim.diagnostic.jump({ count=1 })
+  vim.diagnostic.jump({ count=1 })
 end, { desc = "Go to next error" })
 vim.keymap.set("n", "[e", function()
-    vim.diagnostic.jump({ count=-1 })
+  vim.diagnostic.jump({ count=-1 })
 end, { desc = "Go to previous error" })
 vim.keymap.set("n", "gl", function()
-    vim.diagnostic.open_float()
+  vim.diagnostic.open_float()
 end, { desc = "View diagnostic message" })
 
 vim.keymap.set("n", "<leader>a", "<cmd>AerialNavToggle<cr>", { desc = "Toggle Aerial code outlining" })
 
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<S-k>", function()
+  vim.lsp.buf.hover({
+    border = "rounded",
+  })
+end)
+
+vim.diagnostic.config({
+  float = {
+    border = "rounded", -- Options: "none", "single", "double", "rounded", "solid", "shadow"
+    header = "",        -- Removes the "Diagnostics:" header for a cleaner look
+  },
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking text",
-    group = vim.api.nvim_create_augroup("highlight-on-yank", { clear = true }),
-    callback = function()
-	vim.hl.on_yank()
-    end,
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("highlight-on-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
 })
 
 require("config.lazy")
 
+vim.cmd("colorscheme slate")
+
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("intelephense")
 vim.lsp.enable("vtsls")
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
--- .config/nvim/lua/config/plugins/snacks.lua
+++ .config/nvim/lua/config/plugins/snacks.lua
@@ -1,22 +1,144 @@
+local confirm = function(self)
+    local before = vim.api.nvim_list_bufs()
+    self:action("confirm")()
+    local after = vim.api.nvim_list_bufs()
+
+    if table.concat(before) ~= table.concat(after) and self.meta["is_maximized"] then
+	self:action("toggle_maximize")()
+    end
+end
+local toggleMaximize = function(self)
+    self.meta["is_maximized"] = self.meta["is_maximized"] ~= true
+    self:action("toggle_maximize")()
+end
+
 return {
     {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
 	opts = {
-	    -- bigfile = { enabled = false },
-	    -- dashboard = { enabled = false },
-	    -- explorer = { enabled = false },
-	    -- indent = { enabled = false },
-	    -- input = { enabled = false },
-	    -- picker = { enabled = false },
-	    -- notifier = { enabled = false },
-	    -- quickfile = { enabled = false },
-	    -- scope = { enabled = false },
-	    -- scroll = { enabled = false },
-	    -- statuscolumn = { enabled = false },
-	    -- words = { enabled = false },
-	    image = { enabled = false }
+	    bigfile = { enabled = false },
+	    dashboard = { enabled = false },
+	    indent = { enabled = false },
+	    input = { enabled = false },
+	    notifier = { enabled = false },
+	    quickfile = { enabled = false },
+	    scope = { enabled = false },
+	    scroll = { enabled = false },
+	    statuscolumn = { enabled = false },
+	    words = { enabled = false },
+	    image = { },
+	    explorer = {
+		replace_netrw = true, -- Replace netrw with the snacks explorer
+		trash = false -- Use the system trash when deleting files
+	    },
+	    picker = {
+		sources = {
+		    explorer = {
+			layout = {
+			    hidden = { "input" },
+			    layout = {
+				width = 40,
+			    }
+			},
+			win = {
+			    list = {
+				keys = {
+				    ["q"] = "cancel",
+				    ["s"] = confirm,
+				    ["<2-LeftMouse>"] = confirm,
+				    ["f"] = toggleMaximize,
+				    ["h"] = false,
+				    ["<M-w>"] = false,
+				    ["<C-s>"] = false,
+				    ["<C-v>"] = false,
+				    ["a"] = "explorer_add",
+				    ["Z"] = false,
+				    ["c"] = "explorer_copy",
+				    ["d"] = "explorer_del",
+				    ["]d"] = false,
+				    ["[d"] = false,
+
+				    ["]e"] = false,
+				    ["[e"] = false,
+				    ["."] = false,
+				    ["]g"] = false,
+				    ["[g"] = false,
+				    ["m"] = false,
+				    ["o"] = false,
+				    ["r"] = "explorer_rename",
+				    ["<BS>"] = "explorer_up",
+				    ["<CR>"] = "explorer_focus",
+				    ["u"] = false,
+				    ["]w"] = false,
+				    ["[w"] = false,
+				    ["y"] = "explorer_yank",
+
+				    ["i"] = false,
+				    ["<M-d>"] = false,
+				    ["<C-W>J"] = false,
+				    ["<C-W>H"] = false,
+				    ["<C-W>L"] = false,
+				    ["<C-W>K"] = false,
+				    ["G"] = false,
+				    ["<C-N>"] = false,
+				    ["<C-J>"] = false,
+				    ["<Down>"] = false,
+				    ["j"] = false,
+				    ["zb"] = false,
+				    ["zz"] = false,
+				    ["<C-D>"] = false,
+
+				    ["zt"] = false,
+				    ["<C-U>"] = false,
+				    ["gg"] = false,
+				    ["<C-K>"] = false,
+				    ["<C-P>"] = false,
+				    ["<Up>"] = false,
+				    ["k"] = false,
+				    ["<S-CR>"] = false,
+				    ["<Space>/"] = false,
+				    ["<C-F>"] = false,
+				    ["<C-B>"] = false,
+				    ["<C-G>"] = false,
+				    ["<C-Q>"] = false,
+				    ["<C-A>"] = false,
+
+				    ["<Tab>"] = false,
+				    ["<S-Tab>"] = false,
+				    ["<C-C>"] = false,
+				    ["<C-T>"] = false,
+				    ["/"] = false,
+				    ["<M-f>"] = false,
+				    ["?"] = "toggle_help_list",
+				    ["H"] = false,
+				    ["<M-h>"] = false,
+				    ["I"] = false,
+				    ["<M-i>"] = false,
+				    ["<M-m>"] = false,
+				    ["<M-p>"] = false,
+				    ["p"] = "toggle_preview",
+				    ["l"] = false
+				}
+			    },
+			},
+		    }
+		}
+	    },
 	},
+	keys = {
+	    { "<leader>e", function() require("snacks").explorer({
+		follow_file = true,
+		watch = true,
+		hidden = true,
+		ignored = true,
+	    }) end, desc = "Open file explorer" },
+	    { "<leader>ff", function() require("snacks").picker.files() end, desc = "Find file" },
+	    { "<leader>fw", function() require("snacks").picker.grep() end, desc = "Live grep" },
+	    { "<leader>fb", function() require("snacks").picker.buffers() end, desc = "Buffers" },
+	    { "<leader>lr", function() require("snacks").picker.lsp_references() end, desc = "Search LSP references" },
+	    { "gd", function() require("snacks").picker.lsp_definitions() end, desc = "Search LSP definitions" },
+	}
     }
 }
